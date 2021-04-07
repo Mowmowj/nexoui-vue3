ae20856e5cd9960e61ba64ac02e229e20cfcbaeb
shelton cheung <sheltonzhang@xiaoe-tech.com>
2021-04-08 01:10:11 +0800

    feat:add Switch component
    feat:add Switch component

--- src/components/SwitchDemo.vue
+++ src/components/SwitchDemo.vue
@@ -1,65 +1,23 @@
 <template>
-  <div>demo</div>
-  <div>demo</div>
-  <div>demo</div>
-  <div>demo</div>
-  <div>demo</div>
-  <div>demo</div>
-  <div>demo</div>
-  <div>demo</div>
-  <div>demo</div>
-  <div>demo</div>
-  <div>demo</div>
-  <div>demo</div>
-  <div>demo</div>
-  <div>demo</div>
-  <div>demo</div>
-  <div>demo</div>
-  <div>demo</div>
-  <div>demo</div>
-  <div>demo</div>
-  <div>demo</div>
-  <div>demo</div>
-  <div>demo</div>
-  <div>demo</div>
-  <div>demo</div>
-  <div>demo</div>
-  <div>demo</div>
-  <div>demo</div>
-  <div>demo</div>
-  <div>demo</div>
-  <div>demo</div>
-  <div>demo</div>
-  <div>demo</div>
-  <div>demo</div>
-  <div>demo</div>
-  <div>demo</div>
-  <div>demo</div><div>demo</div>
-  <div>demo</div>
-  <div>demo</div>
-  <div>demo</div>
-  <div>demo</div>
-  <div>demo</div>
-  <div>demo</div>
-  <div>demo</div>
-  <div>demo</div>
-  <div>demo</div>
-  <div>demo</div>
-  <div>demo</div>
-  <div>demo</div>
-  <div>demo</div>
-  <div>demo</div>
-  <div>demo</div>
-  <div>demo</div>
-  <div>demo</div>
+    <div>
+        <!-- 三种写法  第三种是vue3 新的 -->
+        <!-- <Switch :value="switchStatus" @nexo=" switchStatus = $event "/> -->
+        <!-- <Switch :value="switchStatus" @update:value=" switchStatus = $event "/> -->
+        <Switch v-model:value="switchStatus"/>
+
+    </div>
 </template>
 
-<script>
+<script lang="ts">
+import { ref } from 'vue'
+
+import Switch from "../lib/Switch.vue";
 export default {
+  components: { Switch },
+  setup(){
+      const switchStatus = ref(true)
+      return { switchStatus }
 
-}
-</script>
-
-<style>
-
-</style>
+  }
+  
+};
---
+++ src/lib/Switch.vue
@@ -0,0 +1,56 @@
+<template>
+<button @click="toggleSwitch"  :class="{checked:value}">
+    <span></span>
+</button>
+</template>
+
+<script lang="ts">
+import { ref } from 'vue'
+export default {
+    props:{
+        value:Boolean
+    },
+    setup(props,context){
+        const toggleSwitch = ()=>{
+        // context.emit('nexo',!props.value)
+        context.emit('update:value',!props.value)
+        //vue2 this.$emit()
+        }
+        return {toggleSwitch}
+    }
+}
+</script>
+
+<style scoped lang="scss">
+$bh:22px;
+$sh:$bh - 4px;
+
+button{
+    height: $bh;
+    width: $bh*2;
+    border: none;
+    background-color : grey;
+    border-radius:$bh/2;
+    position: relative;
+    transition: background-color  500ms;
+}
+span{
+    position: absolute;
+    top: 2px;
+    left: 2px;
+    height: $sh;
+    width: $sh;
+    background: purple;
+    border-radius: $sh/2;
+    transition: left  250ms;
+}
+button:focus{
+    outline: none;
+}
+button.checked{
+    background: palevioletred;
+}
+button.checked >span{
+    left: calc(100% - #{$sh} - 1px);
+}
+</style>
